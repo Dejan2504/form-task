87f6c9094e2205835f6b3bd9a2593fef0afd35aa
Dejan2504 <krajisnikdejan@gmail.com>
2022-12-30 21:12:50 +0100

    Validation done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Login = function() {
-    const [email, setEmail] = useState('');
+    const [isValid, setIsValid] = useState(false);
     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        const trimmed = [...password];
+        const specialChars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        const isUpper = trimmed.some(letter => {return letter === letter.toUpperCase()});
+        const isLower = trimmed.some(letter => {return letter === letter.toLowerCase()});
+        const isNumber = trimmed.some(letter => {return !isNaN(letter)});
+        
+        
+        if(password.length >= 8 && isUpper && isLower && isNumber && specialChars.test(password)){
+            setIsValid(true);
+        }
+        
+    },[password])
+
+    const emailHandler = function(e){
+        setEmail(e.target.value);
+        console.log(e.target.value.length);
+    }
+
+    const passwordHandler = function(e){
+        setPassword(e.target.value);
+        
+    }
 
     return(
-        <div>
-            <input type='email' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-            <input type='password' value={password} onChange={(e) => {setEmail(e.target.value)}} required/>
-        </div>
+        <form>
+            <input type='email' name='email' value={email} onChange={emailHandler} required/>
+            <input type='password' name='password' value={password} onChange={passwordHandler} required/>
+            {isValid ? <p>You are good to go</p> : <p>Error my man</p>}
+        </form>
     )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,39 @@
 import { useState } from "react";
 
 const Register = function() {
+    const users = {email: '', password: ''};
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
+    const emailHandler = function(e){
+        setEmail(e.target.value)
+        console.log(`email: ${e.target.value}`);
+    }
+
+    const passwordHandler = function(e){
+        setPassword(e.target.value)
+        console.log(`pass: ${e.target.value}`);
+    }
+
+    const confirmPasswordHandler = function(e){
+        setConfirmPassword(e.target.value)
+        console.log(`confirmPass: ${e.target.value}`);
+    }
+    
+    const addUser = (e) => {
+        e.preventDefault();
+
+        
+    }
 
     return(
-        <div>
-            <input type='email' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-            <input type='password' value={password} onChange={(e) => {setEmail(e.target.value)}}/>
-            <input type='password' value={confirmPassword} onChange={(e) => {setEmail(e.target.value)}}/>
-        </div>
+        <form onSubmit={addUser}>
+            <input type='email' value={email} onChange={emailHandler}/>
+            <input type='password' value={password} onChange={passwordHandler}/>
+            <input type='password' value={confirmPassword} onChange={confirmPasswordHandler}/>
+            <button>Add user</button>
+        </form>
     )
 }
 
